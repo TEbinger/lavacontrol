d5414b7c291dc66aced08b0b06fde369baa6d22a
TEbinger <thomas.ebinger@gmx.de>
2017-11-14 19:24:57 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 local LAVA_PLACE_DEPTH = -50
 
-function allow_place_lava( pos, player )
+minetest.register_privilege( "lava", "Can place lava at any depth.")
+
+function allow_place_lava( node_pos, player )
         if pos.y > LAVA_PLACE_DEPTH and not minetest.check_player_privs( player, "lava" ) then
                 minetest.chat_send_player( player:get_player_name( ), "You are not allowed to place lava above " .. LAVA_PLACE_DEPTH .. "!" )
                 minetest.log( "action", player:get_player_name( ) .. " tried to place default:lava_source above " .. LAVA_PLACE_DEPTH )
@@ -16,4 +18,4 @@ minetest.override_item( "bucket:bucket_lava", {
 minetest.override_item( "default:lava_source", {
         allow_place = allow_place_lava
 } )
-minetest.register_privilege( "lava", "Can place lava at any depth.")
+
