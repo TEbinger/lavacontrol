6beb101e942b14342f8be9f8a5fcb042d56af8f4
TEbinger <thomas.ebinger@gmx.de>
2017-11-18 23:41:45 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -19,3 +19,22 @@ minetest.override_item( "default:lava_source", {
         on_place = allow_place_lava
 } )
 
+--ab hier kopiert von https://github.com/SmallJoker/minibase_game/blob/master/mods/default/nodes.lua#L535
+
+on_place = function(itemstack, placer, pointed_thing)
+		if not pointed_thing.above then
+			return
+		end
+		if not minetest.is_singleplayer() then
+			local player_name = placer:get_player_name()
+			if pointed_thing.above.y > -5 then
+				minetest.chat_send_player(player_name, "Do not place lava over -5m, that could end really bad!", true)
+				return itemstack
+			end
+			if minetest.is_protected(pointed_thing.above, player_name) then
+				minetest.record_protection_violation(pointed_thing.above, player_name)
+				return itemstack
+			end
+		end
+		return minetest.item_place(itemstack, placer, pointed_thing, 0)
+	end
