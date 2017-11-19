e0633bedadfb38c988da10a2cb8add65c2a15582
TEbinger <thomas.ebinger@gmx.de>
2017-11-19 13:20:05 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,94 +1,68 @@
-minetest.register_node("default:lava_flowing", {
-	description = "Flowing lava",
-	inventory_image = minetest.inventorycube("default_lava.png"),
-	drawtype = "flowingliquid",
-	tiles = {"default_lava.png"},
-	special_tiles = {
-		{
-			name = "default_lava_flowing_animated.png",
-			backface_culling = false,
-			animation = {type="vertical_frames", aspect_w=16, aspect_h=16, length=3.3}
-		},
-		{
-			name = "default_lava_flowing_animated.png",
-			backface_culling = true,
-			animation = {type="vertical_frames", aspect_w=16, aspect_h=16, length=3.3}
-		},
-	},
-	paramtype = "light",
-	paramtype2 = "flowingliquid",
-	light_source = LIGHT_MAX - 1,
-	walkable = false,
-	pointable = false,
-	diggable = false,
-	buildable_to = true,
-	drop = "",
-	drowning = 1,
-	liquidtype = "flowing",
-	liquid_alternative_flowing = "default:lava_flowing",
-	liquid_alternative_source = "default:lava_source",
-	liquid_viscosity = 7,
-	liquid_renewable = false,
-	damage_per_second = 6,
-	post_effect_color = {a=192, r=255, g=64, b=0},
-	groups = {lava=3, liquid=2, hot=3, igniter=1, not_in_creative_inventory=1},
-})
+--weitere interessanter Code: https://github.com/SmallJoker/minibase_game/blob/master/mods/default/nodes.lua#L535
+--Quelle: https://forum.minetest.net/viewtopic.php?p=299612#p299612
 
-minetest.register_node("default:lava_source", {
-	description = "Lava source",
-	stack_max = 10,
-	inventory_image = minetest.inventorycube("default_lava.png"),
-	drawtype = "liquid",
-	tiles = {
-		{
-			name = "default_lava_source_animated.png",
-			animation = {type="vertical_frames", aspect_w=16, aspect_h=16, length=3.0}
-		}
-	},
-	special_tiles = {
-		{
-			name = "default_lava_source_animated.png",
-			backface_culling = false,
-			animation = {type="vertical_frames", aspect_w=16, aspect_h=16, length=3.0},
-		}
-	},
-	paramtype = "light",
-	light_source = LIGHT_MAX - 1,
-	walkable = false,
-	pointable = false,
-	diggable = false,
-	buildable_to = true,
-	drop = "",
-	drowning = 1,
-	liquidtype = "source",
-	liquid_alternative_flowing = "default:lava_flowing",
-	liquid_alternative_source = "default:lava_source",
-	liquid_viscosity = 7,
-	liquid_renewable = false,
-	damage_per_second = 8,
-	post_effect_color = {a=192, r=255, g=64, b=0},
-	groups = {lava=3, liquid=2, hot=3, igniter=1},
-	on_place = function(itemstack, placer, pointed_thing)
-		if not pointed_thing.above then
-			return
-		end
-		if not minetest.is_singleplayer() then
-			local player_name = placer:get_player_name()
-			if pointed_thing.above.y > -5 then
-				minetest.chat_send_player(player_name, "Do not place lava over -5m, that could end really bad!", true)
-				return itemstack
-			end
-			if minetest.is_protected(pointed_thing.above, player_name) then
-				minetest.record_protection_violation(pointed_thing.above, player_name)
-				return itemstack
-			end
-		end
-		return minetest.item_place(itemstack, placer, pointed_thing, 0)
-	end
-})
+minetest.register_privilege( "lava", "Can place lava at any depth.")
+local old_register_node = minetest.register_node
+local old_register_craftitem = minetest.register_craftitem
+
+minetest.register_craftitem = function ( name, fields )
+   if fields.on_place then
+      fields._on_place = fields.on_place
+      fields.on_place = function( itemstack, player, pointed_thing )
+         local pos = pointed_thing.under
+         local node = minetest.get_node( pos )
+
+         -- allow on_rightclick callback of pointed_thing to intercept item placement
+         if minetest.registered_nodes[ node.name ].on_rightclick then
+            minetest.registered_nodes[ node.name ].on_rightclick( pos, node, player, itemstack )
+         -- otherwise placement is dependent on anti-grief rules of this item (if hook is defined)
+         elseif not fields.allow_place or fields.allow_place( pos, player ) then
+            return fields._on_place( itemstack, player, pointed_thing )
+         end
+         return itemstack
+      end
+   end
+   old_register_craftitem( name, fields )
+end
+
+minetest.register_node = function ( name, fields )
+   if fields.on_place and fields.on_place ~= minetest.rotate_node then
+      fields._on_place = fields.on_place
+      fields.on_place = function( itemstack, player, pointed_thing )
+         local pos = pointed_thing.under
+         local node = minetest.get_node( pos )
+
+         -- allow on_rightclick callback of pointed_thing to intercept item placement
+         if minetest.registered_nodes[ node.name ].on_rightclick then
+            minetest.registered_nodes[ node.name ].on_rightclick( pos, node, player, itemstack )
+         -- otherwise placement is dependent on anti-grief rules of this item (if hook is defined)
+         elseif not fields.allow_place or fields.allow_place( pos, player ) then
+            return fields._on_place( itemstack, player, pointed_thing )
+         end
+         return itemstack
+      end
+   end
+   if fields.on_punch then
+      fields._on_punch = fields.on_punch
+      fields.on_punch = function( pos, node, player )
+         -- punching is dependent on anti-grief rules of this item (currently only used by tnt)
+         if not fields.allow_punch or fields.allow_punch( pos, player ) then
+            fields._on_punch( pos, node, player )
+         end
+      end
+   end
+   old_register_node( name, fields )
+end
+
+local LAVA_PLACE_DEPTH = -250
+
+local function allow_place_lava( node_pos, player )
+        if node_pos.y > LAVA_PLACE_DEPTH and not minetest.check_player_privs( player, "lava" ) then
+                minetest.chat_send_player( player:get_player_name( ), "You are not allowed to place explosives above " .. LAVA_PLACE_DEPTH .. "!" )
+                minetest.log( "action", player:get_player_name( ) .. " tried to place tnt:tnt above " .. LAVA_PLACE_DEPTH )
+                return false
+        end
+        return true
+end
 
 
-
-
-
---Code kopiert von https://github.com/SmallJoker/minibase_game/blob/master/mods/default/nodes.lua#L535
